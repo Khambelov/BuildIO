40afc528c3d0a60501140fa0ed54adf88f833172
Khambelov <lokinko89@gmail.com>
2020-02-01 17:16:42 +0300

    Add builds container. Add House Class.

---
+++ Assets/Scripts/BuildContainer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class BuildContainer : MonoBehaviour
+{
+	public static BuildContainer Instance { get { return instance; } }
+	private static BuildContainer instance;
+
+	public Sprite DestroyHouse;
+	public Sprite BuildedHouse;
+	public GameObject BuildingSmoke;
+
+	private List<House> houses;
+
+	void Awake()
+	{
+		instance = this;
+
+		houses = new List<House>();
+	}
+
+	public void AddNewHouse(House house)
+	{
+		houses.Add(house);
+	}
+
+	public House FindNearestHouse(Vector3 position)
+	{
+		return houses.OrderBy(h => Vector3.Distance(position, h.transform.position)).FirstOrDefault();
+	}
+
+	public void DropBuildedHouse(House house)
+	{
+		houses.Remove(house);
+	}
+}
---
+++ Assets/Scripts/House.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour
+{
+	public EHouseState HouseState;
+	public bool BuildIsCancelled { get; }
+
+	private SpriteRenderer render;
+
+	private int coins;
+	private float buildProgress;
+
+	private GameObject owner;
+
+	void Start()
+	{
+		HouseState = EHouseState.Destroyed;
+		BuildContainer.Instance.AddNewHouse(this);
+		ChangeSprite();
+		owner = null;
+		buildProgress = 0f;
+		coins = 1000;
+	}
+
+	public void StartBuilding(float buildSpeed, GameObject team)
+	{
+		if (HouseState == EHouseState.Destroyed)
+		{
+			owner = team;
+
+			HouseState = EHouseState.Building;
+			ChangeSprite();
+
+			StartCoroutine(BuildingHouse(buildSpeed));
+		}
+	}
+
+	public void CancelBuildings()
+	{
+		StopAllCoroutines();
+
+		HouseState = EHouseState.Destroyed;
+		buildProgress = 0f;
+		ChangeSprite();
+	}
+
+	public int GetReward()
+	{
+		return coins;
+	}
+
+	private void ChangeSprite()
+	{
+		if (HouseState == EHouseState.Destroyed)
+			render.sprite = BuildContainer.Instance.DestroyHouse;
+		if (HouseState == EHouseState.Builded)
+			render.sprite = BuildContainer.Instance.BuildedHouse;
+
+	}
+
+	private IEnumerator BuildingHouse(float buildSpeed)
+	{
+		var smoke = Instantiate(BuildContainer.Instance.BuildingSmoke, Vector3.zero, Quaternion.identity, transform);
+
+		while (buildProgress < 100f)
+		{
+			buildProgress += buildSpeed;
+
+			yield return new WaitForSeconds(1f);
+		}
+
+		if (buildProgress >= 100f)
+		{
+			buildProgress = 100f;
+
+			HouseState = EHouseState.Builded;
+			BuildContainer.Instance.DropBuildedHouse(this);
+			ChangeSprite();
+
+			Destroy(smoke);
+
+			//Сделать что-то с персонажем, например, флажок покрасить
+		}
+
+		yield return null;
+	}
+}
